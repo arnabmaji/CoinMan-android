5cb7ada2fd00aa082472ca3bda34ad7a82577009
Arnab Maji <arnabmaji1999@gmail.com>
2020-02-20 23:22:20 +0530

    draw single man texture in middle of the screen

--- core/src/io/github/arnabmaji19/coinman/CoinMan.java
+++ core/src/io/github/arnabmaji19/coinman/CoinMan.java
@@ -8,25 +8,44 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class CoinMan extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Texture backgroundTexture;
+	private Texture[] man;
 
 	private int height;
 	private int width;
+	private int manHeight;
+	private int manWidth;
 
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+        // get screen height and width
+        height = Gdx.graphics.getHeight();
+        width = Gdx.graphics.getWidth();
 		backgroundTexture = new Texture("bg.png"); // create background image to display on screen
-		// get screen height and width
-		height = Gdx.graphics.getHeight();
-		width = Gdx.graphics.getWidth();
+        man = new Texture[4]; // create all graphics assets for the man
+        // set all types of man textures
+        man[0] = new Texture("frame-1.png");
+        man[1] = new Texture("frame-2.png");
+        man[2] = new Texture("frame-3.png");
+        man[3] = new Texture("frame-4.png");
+
+        // get height and width of man texture
+        manHeight = man[0].getHeight();
+        manWidth = man[0].getWidth();
+
 	}
 
 	@Override
 	public void render () {
 		batch.begin();
+
 		batch.draw(backgroundTexture, 0, 0, width, height); // draw background image to the screen
-		batch.end();
+
+        //draw man in the middle of the screen
+        batch.draw(man[0], (width - manWidth)/2, (height - manHeight)/2);
+
+        batch.end();
 	}
 	
 	@Override
