84f245704b0423d422502a17bbf099367c010ead
Arnab Maji <arnabmaji1999@gmail.com>
2020-02-20 23:15:54 +0530

    draw game background image

--- core/src/io/github/arnabmaji19/coinman/CoinMan.java
+++ core/src/io/github/arnabmaji19/coinman/CoinMan.java
@@ -1,19 +1,31 @@
 package io.github.arnabmaji19.coinman;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class CoinMan extends ApplicationAdapter {
-	SpriteBatch batch;
+	private SpriteBatch batch;
+	private Texture backgroundTexture;
+
+	private int height;
+	private int width;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		backgroundTexture = new Texture("bg.png"); // create background image to display on screen
+		// get screen height and width
+		height = Gdx.graphics.getHeight();
+		width = Gdx.graphics.getWidth();
 	}
 
 	@Override
 	public void render () {
 		batch.begin();
+		batch.draw(backgroundTexture, 0, 0, width, height); // draw background image to the screen
 		batch.end();
 	}
 	
